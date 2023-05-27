66878772cc9191f80d1fd6c420cb6d57426cecf6
Ascic, Maid <ascija111@gmail.com>
2023-05-27 14:33:09 +0200

    File resturctureing

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+if [ "$UID" = "0" ]; then
+    echo 'This should not be run as root'
+    exit 101
+fi
+
+NAME=Vaktija\@ascija.ba
+
+
+function make-local-install {
+    DEST=~/.local/share/gnome-shell/extensions/$NAME
+
+    echo 'Installing...'
+    if [ ! -d $DEST ]; then
+        mkdir $DEST
+    else
+      rm -r $DEST
+      mkdir $DEST
+    fi
+
+    cp -r src/* $DEST/
+
+    echo 'Done'
+}
+
+function usage() {
+    echo 'Usage: ./install.sh COMMAND'
+    echo 'COMMAND:'
+    echo "  local-install  install the extension in the user's home directory"
+    echo '                 under ~/.local'
+}
+
+case "$1" in
+    "local-install" )
+        make-local-install
+        ;;
+
+    * )
+        usage
+        ;;
+esac
+exit
